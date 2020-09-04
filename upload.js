cf6d651391c87f168e96ce2f22baac2f06cc0e8c
Daniel Nolan <DanNolan99@gmail.com>
2020-09-04 18:37:35 -0400

    update to latest api package

--- upload.js
+++ upload.js
@@ -1,20 +1,10 @@
-import ipfsAPI from 'ipfs-api';
+const ipfsApi = require("ipfs-http-client");
 
-const ipfs = new ipfsAPI({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https'
-});
+const ipfs = ipfsApi("https://ipfs.infura.io:5001");
+// or, if you have a local node running:
+// const ipfs = ipfsApi("http://127.0.0.1:5001");
 
-export default function ipfsUpload(buffer) {
-  return new Promise(function(resolve, reject) {
-    ipfs.add(buffer, (err, ipfsHash) => {
-      if (err) {
-        console.log(err);
-        reject(err);
-      } else {
-        resolve(`https://gateway.ipfs.io/ipfs/${ipfsHash[0].hash}`);
-      }
-    });
-  });
+export default async function ipfsUpload(buffer) {
+  const result = await ipfs.add(buffer);
+  return `https://gateway.ipfs.io/ipfs/${result.path}`;
 }
