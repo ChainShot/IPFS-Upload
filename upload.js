05611d60bfa9f6371eac8f655ae89507cfd60a38
Daniel Nolan <DanNolan99@gmail.com>
2021-08-18 14:44:29 -0400

    update ipfs-http-client

--- upload.js
+++ upload.js
@@ -1,8 +1,8 @@
-const ipfsApi = require("ipfs-http-client");
+const { create } = require("ipfs-http-client");
 
-const ipfs = ipfsApi("https://ipfs.infura.io:5001");
+const ipfs = create("https://ipfs.infura.io:5001");
 // or, if you have a local node running:
-// const ipfs = ipfsApi("http://127.0.0.1:5001");
+// const ipfs = create("http://127.0.0.1:5001");
 
 export default async function ipfsUpload(buffer) {
   const result = await ipfs.add(buffer);
